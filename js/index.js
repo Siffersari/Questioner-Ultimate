9d3c5a472148cebc2f338befb003b73afd477728
LeewelKarani <Leewelkarani@gmail.com>
2019-01-23 15:43:06 +0300

    [Feature #163374300] Add pop up

--- js/index.js
+++ js/index.js
@@ -9,25 +9,23 @@ function signUp(event) {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       firstname: document.getElementById('signfirst').value,
-      othername: 'N/A',
+      othername: document.getElementById('signother').value,
       lastname: document.getElementById('signlast').value,
       username: document.getElementById('signuname').value,
       email: document.getElementById('signmail').value,
       password: document.getElementById('signpwd').value,
-      phoneNumber: 'Pass for now',
+      phoneNumber: document.getElementById('signno').value,
     }),
   })
     .then(async (resp) => {
-      console.log(resp);
       if (resp.ok) {
         const data = await resp.json();
-        console.log(data);
+
         document.getElementById('signresp').innerHTML = JSON.stringify(data.data[0].message);
       }
       if (resp.status !== 201) {
         const dataOne = await resp.json();
-        console.log(dataOne);
-        document.getElementById('signresp').innerHTML = JSON.stringify(dataOne.error);
+        window.alert(JSON.stringify(dataOne.error));
       }
     });
 }
