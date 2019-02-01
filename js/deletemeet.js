85e02ed88bdda6e4ef27732edba934f9bb508144
LeewelKarani <Leewelkarani@gmail.com>
2019-02-01 13:42:59 +0300

    [Finishes #163374805] consume endpoint that deletes a meetup

--- js/deletemeet.js
+++ js/deletemeet.js
@@ -1,24 +1,45 @@
 const deletemeetbtn = document.getElementById('deletemeetup');
 
 
-function getMeetup(event) {
+function deleteMeetup(event) {
   event.preventDefault();
 
-  fetch('https://questioner-apiv2-siffersari.herokuapp.com/api/v2/meetups', {
+  const topic = document.getElementById('meetuptopic').innerText;
+
+  const location = document.getElementById('venue').innerText;
+
+  const url = `https://questioner-apiv2-siffersari.herokuapp.com/api/v2/meetups/${topic}/${location}`;
+
+  fetch(url, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${localStorage.getItem('token')}` },
-    body: JSON.stringify({
-      location: document.getElementById('venue').innerText,
-      topic: document.getElementById('meetuptopic').innerText,
-
-    }),
   })
     .then(async (resp) => {
       if (resp.ok) {
         const data = await resp.json();
-        console.log(data);
+        const meetupId = data.data[0].id;
+
+        fetch(`https://questioner-apiv2-siffersari.herokuapp.com/api/v2/meetups/${meetupId}`, {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${localStorage.getItem('token')}` },
+        })
+          .then(async (resp) => {
+            if (resp.ok) {
+              const data = await resp.json();
+              console.log(data);
+              document.getElementById('dmeetupresp').innerHTML = 'Meetup deleted successfully';
+
+              setTimeout(() => {
+                window.location.href = 'meetups.html';
+              }, 2000);
+            }
+            if (resp.status !== 200) {
+              const dataOne = await resp.json();
+              window.alert(JSON.stringify(dataOne.error));
+            }
+          });
       }
-      if (resp.status !== 201) {
+      if (resp.status !== 200) {
         const dataOne = await resp.json();
         window.alert(JSON.stringify(dataOne.error));
       }
@@ -26,4 +47,4 @@ function getMeetup(event) {
 }
 
 
-deletemeetbtn.addEventListener('click', getMeetup);
+deletemeetbtn.addEventListener('click', deleteMeetup);
