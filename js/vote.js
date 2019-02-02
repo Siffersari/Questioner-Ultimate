da2d3cbd3d55c0a6b110206199af8dfe5d028f36
LeewelKarani <Leewelkarani@gmail.com>
2019-02-02 21:12:10 +0300

    [Finishes #163374706] Consume API

---
+++ js/vote.js
@@ -0,0 +1,48 @@
+const upvote = document.getElementById('upvote');
+const downvote = document.getElementById('downvote');
+
+function upvoteQuestion(event) {
+  event.preventDefault();
+  const query = window.location.search.substring(1);
+  const questionId = query.split('=')[1];
+
+  fetch(`https://questioner-apiv2-siffersari.herokuapp.com/api/v2/questions/${questionId}/upvote`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${localStorage.getItem('token')}` },
+  })
+    .then(async (resp) => {
+      if (resp.ok) {
+        const data = await resp.json();
+
+        document.getElementById('questionvotes').innerText = data.data[0].votes;
+      }
+      if (resp.status !== 200) {
+        const dataOne = await resp.json();
+        window.alert(JSON.stringify(dataOne.error));
+      }
+    });
+}
+
+function downvoteQuestion(event) {
+  event.preventDefault();
+  const query = window.location.search.substring(1);
+  const questionId = query.split('=')[1];
+
+  fetch(`https://questioner-apiv2-siffersari.herokuapp.com/api/v2/questions/${questionId}/downvote`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${localStorage.getItem('token')}` },
+  })
+    .then(async (resp) => {
+      if (resp.ok) {
+        const data = await resp.json();
+        document.getElementById('questionvotes').innerText = data.data[0].votes;
+      }
+      if (resp.status !== 200) {
+        const dataOne = await resp.json();
+        window.alert(JSON.stringify(dataOne.error));
+      }
+    });
+}
+
+upvote.addEventListener('click', upvoteQuestion);
+downvote.addEventListener('click', downvoteQuestion);
