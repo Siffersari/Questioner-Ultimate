bf50c51a48acd27980acfa0ed427852cf0618fe8
Leewel Karani <Leewelkarani@gmail.com>
2019-03-02 18:07:23 +0300

    [Bug #164133677] set browser visibility true

--- js/spec/register.spec.js
+++ js/spec/register.spec.js
@@ -23,6 +23,11 @@ require('babel-polyfill');
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
+    
+
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`],
   });
   page = await browser.newPage();
   await page.setViewport({ width, height });
@@ -30,7 +35,7 @@ beforeAll(async () => {
 
 require('babel-polyfill');
 
-afterAll(async () => {
+afterAll(() => {
   browser.close();
 });
 
